36f1563c0d71cda3dab096b5ab3b0a4a3f4440d7
zhurainyk <zhurainyk@163.com>
2019-09-18 14:42:59 +0800

    upload files

---
+++ src/views/Chat/index.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="Chat">
+        <NavBarPublic keyword="客服中心"/>
+        <div data-v-7da18f7e="" id="write" class="is-node"><p data-v-7da18f7e=""> 数据编码解码及历史管理</p>
+            <p data-v-7da18f7e="">​ H5多文件上传</p>
+            <p data-v-7da18f7e="">​ H5本地存储-离线存储-web多线程-server</p>
+            <p data-v-7da18f7e=""> canvas特效</p>
+            <p data-v-7da18f7e=""> 移动端项目适配方案</p>
+            <p data-v-7da18f7e="">​ 原生JS封装移动端网页轮播图上下滑屏特效插件</p>
+            <p data-v-7da18f7e="">​ 原生JS封装插件Android / IOS移动端手势操作插件</p>
+            <p data-v-7da18f7e="">​ 获取用户地理位置应用、百度地图 / 高德地图API应用</p>
+            <p data-v-7da18f7e="">​ echarts / highcharts 高级图表应用</p>
+            <p data-v-7da18f7e="">​ 前端网页跨服务器和PHP后端数据交互</p>
+            <p data-v-7da18f7e="">​ 整个网站项目打包Android APP</p>
+            <p data-v-7da18f7e="">​ 动态音乐播放器Android APP开发</p>
+            <p data-v-7da18f7e="">​ ajax上传调取摄像头摄像、拍摄、语音的资源</p>
+            <p data-v-7da18f7e="">​ 模拟视频直播系统、拍照、下载、水印、美颜</p>
+            <p data-v-7da18f7e="">​ Ajax2请求多媒体资源、播放、变声、封装音频音波图</p>
+            <p data-v-7da18f7e="">​ 3D真实环绕音效</p>
+            <p data-v-7da18f7e="">​ 语音识别、文字播报成语音，语音翻译成文字</p>
+            <p data-v-7da18f7e="">​ 真实webSocket多人聊天室功能</p>
+            <p data-v-7da18f7e=""> 网页版真实闹铃功能</p>
+            <p data-v-7da18f7e=""> 重力感应与陀螺仪应用</p>
+            <p data-v-7da18f7e=""> 阿里云Linux服务器node环境配置</p>
+            <p data-v-7da18f7e=""> 阿里云Linux服务器php运行环境配置</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Chat"
+    }
+</script>
+
+<style lang='scss' scoped>
+    .Chat {
+        background-color: hotpink;
+        .is-node{
+            margin-top:84px;
+            p{
+                font-size:32px;
+                margin-bottom:20px;
+            }
+            p:last-child{
+                margin-bottom:0;
+            }
+        }
+    }
+</style>
